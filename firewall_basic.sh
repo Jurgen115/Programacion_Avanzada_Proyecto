2c6290007af8bbfff64300b1496129147113c4be
Jurgen115 <jbrenes90056@ufide.ac.cr>
2025-10-15 20:38:38 -0600

    Create firewall_basic.sh

---
+++ firewall_basic.sh
@@ -0,0 +1,16 @@
+
+echo "Configurando reglas básicas de firewall..."
+
+sudo ufw --force reset
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+
+sudo ufw --force enable
+
+echo "Firewall configurado correctamente."
+sudo ufw status verbose
